5e0b9494cc547893e7d2dc5b85020192e4200e40
KushwahaPawan1 <kuswahapawan60@gmail.com>
2026-06-22 13:49:08 +0530

    Added colored markers

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,81 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition(
-        (position)=>{
-        const { latitude,longitude } =position.coords;
-        socket.emit("send-location",{latitude,longitude});
+const map = L.map("map").setView([0, 0], 16);
 
-    },
-    (error)=>{
-        console.error(error);
-    },
-   {
-    enableHighAccuracy:true,
-    timeout:5000,
-    maximumAge:0,
-   }
- );
-
-}
-
-const map = L.map("map").setView([0,0],16);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"OpenStreetMap"
-}).addTo(map)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "OpenStreetMap"
+}).addTo(map);
 
 const markers = {};
 
-socket.on("receive-location",(data)=>{
-    const {id,latitude,longitude}=data;
-    map.setView([latitude,longitude]);
-    if(markers[id]){
-        markers[id].setLatLng([latitude,longitude]);
-    }
-    else{
-        markers[id] = L.marker([latitude,longitude]).addTo(map);
+// Green Marker (Current User)
+const greenIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+// Red Marker (Other Users)
+const redIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+// Send Location
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+
+            socket.emit("send-location", {
+                latitude,
+                longitude
+            });
+        },
+        (error) => {
+            console.error(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    );
+}
+
+// Receive Location
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+
+    map.setView([latitude, longitude]);
+
+    // Apna marker green, baaki sab red
+    const icon = (id === socket.id) ? greenIcon : redIcon;
+
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude], {
+            icon: icon
+        }).addTo(map);
+
+        markers[id].bindPopup(
+            id === socket.id ? "🟢 You" : "🔴 Other User"
+        );
     }
 });
 
-socket.on("user-disconnected",(id)=>{
-    if(markers[id]){
+// Remove disconnected users
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
     }
 });
-
