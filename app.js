3d2d11bc3c778a799cb7f8b6f4a25394bcb5eef6
KushwahaPawan1 <kuswahapawan60@gmail.com>
2026-06-22 12:54:49 +0530

    Real Time Tracker Project

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require('express')
+const app = express();
+
+const http = require("http")
+const path = require("path");
+const socketio = require("socket.io")
+
+const server = http.createServer(app);
+
+const io=socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",(socket)=>{
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{ id:socket.id,  ...data  });
+    });
+
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected",socket.id);
+    })
+})
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
